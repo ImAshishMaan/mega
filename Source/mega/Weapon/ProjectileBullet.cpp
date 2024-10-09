8edb0cc973b5a64f4ba28756d93255f0c29c1d3e
Ashish <ashishmaan22@gmail.com>
2024-10-09 12:02:23 +0530

    move physics object on hit (mass dependent)

--- Source/mega/Weapon/ProjectileBullet.cpp
+++ Source/mega/Weapon/ProjectileBullet.cpp
@@ -20,6 +20,20 @@ void AProjectileBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, 
 			UGameplayStatics::ApplyDamage(OtherActor, Damage, OwnerController, BulletOwner, UDamageType::StaticClass());
 		}
 	}
+
+	if (OtherActor && OtherActor != this && OtherActor != BulletOwner) {
+		if (OtherComp && OtherComp->IsSimulatingPhysics() && OtherComp->GetMass() > 0.0f) {
+			// Remember to set object mass in the Physics Asset
+			float Mass = OtherComp->GetMass();
+			float ImpulseStrength = 20000.0f;  
+			FVector ImpulseDirection = Hit.ImpactNormal * -1.0f;
+
+			FVector Impulse = ImpulseDirection * ImpulseStrength;
+			Impulse = Impulse / Mass;
+			
+			OtherComp->AddImpulseAtLocation(Impulse, Hit.ImpactPoint);
+		}
+	}
 	
 	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
 }
