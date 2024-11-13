320525d2c7e428a6e958547901096e5b4e63a98d
Ashish <ashishmaan22@gmail.com>
2024-11-13 18:30:45 +0530

    Dash Direction

--- Source/mega/Actions/Action_DashAbility.cpp
+++ Source/mega/Actions/Action_DashAbility.cpp
@@ -1,20 +1,26 @@
 #include "Action_DashAbility.h"
 #include "GameFramework/Character.h"
+#include "mega/Anim/MegaAnimInstance.h"
+#include "mega/Character/MegaCharacter.h"
 
 void UAction_DashAbility::StartAction_Implementation(AActor* Instigator) {
 	Super::StartAction_Implementation(Instigator);
 
 	// Find movement Direction and play animation
-	ACharacter* Character = Cast<ACharacter>(Instigator);
-	if(Character) {
-		if(Character->GetVelocity().X > 0.0f) {
-			Character->PlayAnimMontage(DashRightAnim);
-		} else if(Character->GetVelocity().X < 0.0f) {
-			Character->PlayAnimMontage(DashLeftAnim);
-		} else if(Character->GetVelocity().Y > 0.0f) {
-			Character->PlayAnimMontage(DashForwardAnim);
-		} else if(Character->GetVelocity().Y < 0.0f) {
-			Character->PlayAnimMontage(DashBackwardAnim);
+	AMegaCharacter* MegaCharacter = Cast<AMegaCharacter>(Instigator);
+	if(MegaCharacter) {
+		UMegaAnimInstance* AnimInstance = Cast<UMegaAnimInstance>(MegaCharacter->GetMesh()->GetAnimInstance());
+		if(AnimInstance) {
+			ELocomotionDirections Direction = AnimInstance->AccelerationLocomotionDirection;
+			if(Direction == ELocomotionDirections::Forward) {
+				MegaCharacter->PlayAnimMontage(DashForwardAnim);
+			} else if(Direction == ELocomotionDirections::Backward) {
+				MegaCharacter->PlayAnimMontage(DashBackwardAnim);
+			} else if(Direction == ELocomotionDirections::Left) {
+				MegaCharacter->PlayAnimMontage(DashLeftAnim);
+			} else if(Direction == ELocomotionDirections::Right) {
+				MegaCharacter->PlayAnimMontage(DashRightAnim);
+			}
 		}
 	}
 
